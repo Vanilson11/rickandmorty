e6a1c308039f7bd1bfc5c2335393b10607770437
Vanilson Sousa <vanilsonped7@gmail.com>
2024-02-29 15:14:30 -0300

    uptade header responsive for 700px

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -4,10 +4,10 @@ btnToggle.forEach(btn => {
   btn.addEventListener("click", (e) => {
     const toggle = document.documentElement.classList.toggle('mobal');
     if(toggle){
-      const nav = document.querySelector('nav');
+      const nav = document.querySelector('.header-mobile');
       nav.style.display = 'flex';
     } else {
-      const nav = document.querySelector('nav');
+      const nav = document.querySelector('.header-mobile');
       nav.style.display = 'none';
     }
   });
